9a585a37db2b1d7eec8dd224f0f551a5be2d71a3
vovaanik <vovaanik@yandex.ru>
2020-09-08 14:24:05 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int price = 1600; // Zdesy vvodim lyuboe tseloe chislo
+            int mile = 20;
+            int bonusMile = price / mile;
+            System.out.println(bonusMile);
+    }
+}
